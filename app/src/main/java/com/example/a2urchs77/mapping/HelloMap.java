a96e40ea4391332f9c27504fe447b0773699ed11
Savannah2702 <princess_savie_xxx@hotmail.co.uk>
2017-02-09 10:46:30 +0000

    added cylce map view options and menu

--- app/src/main/java/com/example/a2urchs77/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2urchs77/mapping/HelloMap.java
@@ -12,6 +12,15 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
 
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import android.content.Intent;
+
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+
 public class HelloMap extends Activity implements View.OnClickListener
 {
      MapView mv;
@@ -52,4 +61,48 @@ public class HelloMap extends Activity implements View.OnClickListener
         mv.getController().setCenter(new GeoPoint(newLatitude,newLongitude));
 
     }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
+
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            // react to the menu item being selected...
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivityForResult(intent,0);
+            return true;
+        }
+        return false;
+    }
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+
+        if(requestCode==0)
+        {
+
+            if (resultCode==RESULT_OK)
+            {
+                Bundle extras=intent.getExtras();
+                boolean cyclemap = extras.getBoolean("com.example.cyclemap");
+                if(cyclemap==true)
+                {
+                    mv.setTileSource(TileSourceFactory.CYCLEMAP);
+                }
+                else
+                {
+                    mv.getTileProvider().setTileSource(TileSourceFactory.MAPNIK);
+                }
+            }
+        }
+    }
+
 }
