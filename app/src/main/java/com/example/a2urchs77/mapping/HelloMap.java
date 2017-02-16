fefec518e0bf269c106cef4bb3161858fd6bbd65
2urchs77 <2urchs77@solent.ac.uk>
2017-02-16 10:50:59 +0000

    third activity mostly done

--- app/src/main/java/com/example/a2urchs77/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2urchs77/mapping/HelloMap.java
@@ -1,16 +1,15 @@
 package com.example.a2urchs77.mapping;
 
 import android.app.Activity;
+
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
-
 
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -21,12 +20,10 @@ import android.content.Intent;
 
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 
-public class HelloMap extends Activity implements View.OnClickListener
-{
-     MapView mv;
+public class HelloMap extends Activity implements View.OnClickListener {
+    MapView mv;
 
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -39,70 +36,67 @@ public class HelloMap extends Activity implements View.OnClickListener
         Configuration.getInstance().load
                 (this, PreferenceManager.getDefaultSharedPreferences(this));
 
-        mv = (MapView)findViewById(R.id.map1);
+        mv = (MapView) findViewById(R.id.map1);
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
-        mv.getController().setCenter(new GeoPoint(51.05,-0.72));
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
+
     }
 
     @Override
-    public void onClick(View view) {
-        // retrieve latitude
-        EditText latitude1 = (EditText) findViewById(R.id.latitude);
-        String latitude2 = latitude1.getText().toString();
-        double newLatitude =Double.parseDouble(latitude2);
+     public void onClick(View view) {
 
-        // retrieve longitude
-        EditText longitude1 = (EditText) findViewById(R.id.longitude);
-        String longitude2 = longitude1.getText().toString();
-        double newLongitude = Double.parseDouble(longitude2);
+        }
 
-        // set map to new position
-        mv.getController().setCenter(new GeoPoint(newLatitude,newLongitude));
-
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        MenuInflater inflater=getMenuInflater();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_hello_map, menu);
         return true;
     }
 
 
-
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        if(item.getItemId() == R.id.choosemap)
-        {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.choosemap) {
             // react to the menu item being selected...
-            Intent intent = new Intent(this,MapChooseActivity.class);
-            startActivityForResult(intent,0);
+            Intent intent = new Intent(this, MapChooseActivity.class);
+            startActivityForResult(intent, 0);
+            return true;
+        }
+
+
+        if (item.getItemId() == R.id.setlocation) {
+            Intent intent2 = new Intent(this, SetLocation.class);
+            startActivityForResult(intent2, 1);
             return true;
         }
         return false;
     }
 
-    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
 
-        if(requestCode==0)
-        {
+        if (requestCode == 0) {
 
-            if (resultCode==RESULT_OK)
-            {
-                Bundle extras=intent.getExtras();
+            if (resultCode == RESULT_OK) {
+                Bundle extras = intent.getExtras();
                 boolean cyclemap = extras.getBoolean("com.example.cyclemap");
-                if(cyclemap==true)
-                {
+                if (cyclemap == true) {
                     mv.setTileSource(TileSourceFactory.CYCLEMAP);
-                }
-                else
-                {
+                } else {
                     mv.getTileProvider().setTileSource(TileSourceFactory.MAPNIK);
                 }
+
+            }
+        }
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Bundle extras = intent.getExtras();
+                double newLatitude = extras.getDouble("com.example.newLatitude");
+                double newLongitude = extras.getDouble("com.example.newLongitude");
+                mv.getController().setCenter(new GeoPoint(newLatitude, newLongitude));
             }
         }
     }
 
-}
+            }
+
